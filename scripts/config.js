885fff6634d47bd964ea9d870adb0dc01e9908f8
Chris Sullivan <chrissullivan.dev@gmail.com>
2021-02-24 23:23:17 -0800

    Initial commits

---
+++ scripts/config.js
@@ -0,0 +1,16 @@
+
+module.exports = [
+  {
+    "id": "fi",
+    "name": "Feather",
+    url: "https://feathericons.com/",
+    license: "MIT",
+    license: "https://github.com/feathericons/feather/blob/master/LICENSE",
+    "files": [
+      
+    ],
+    "steps": [
+
+    ]
+  }
+];
